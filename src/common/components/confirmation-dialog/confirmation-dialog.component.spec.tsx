91b1715ae2633060ca2e5b6677558dda867fa723
PabloLabG <pablico@gmail.com>
2022-03-06 11:24:00 +0100

    test component

---
+++ src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+
+describe('Confirmation-dialog component specs', () => {
+  it('View dialog component', () => {
+    // Arrange
+    interface LabelProps {
+      closeButton: string;
+      acceptButton: string;
+    }
+
+    interface Props {
+      isOpen: boolean;
+      onAccept: () => void;
+      onClose: () => void;
+      title: string | React.ReactNode;
+      labels: LabelProps;
+    }
+
+    const labelProps: LabelProps = {
+      closeButton: 'Close',
+      acceptButton: 'Accept',
+    };
+
+    const dialoProps: Props = {
+      isOpen: true,
+      onAccept: () => void {},
+      onClose: () => void {},
+      title: 'Prueba title dialog',
+      labels: labelProps,
+    };
+
+    // Act
+    render(
+      <ConfirmationDialogComponent {...dialoProps}>
+        Prueba cuerpo dialog
+      </ConfirmationDialogComponent>
+    );
+
+    // Assert
+    const dialogElement = screen.getByRole('dialog');
+    const titleElement = screen.getByRole('heading', {
+      level: 2,
+      name: 'Prueba title dialog',
+    });
+
+    const bodyElement = screen.getByText('Prueba cuerpo dialog');
+
+    expect(dialogElement).not.toBeNull();
+    expect(dialogElement.tagName).toEqual('DIV');
+    expect(dialogElement).toBeInTheDocument();
+
+    //titulo
+    expect(titleElement).toBeInTheDocument();
+
+    expect(bodyElement).toBeInTheDocument();
+  });
+});
